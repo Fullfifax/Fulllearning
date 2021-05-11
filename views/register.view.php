04277132d96f723879f899bf3600180fbb60d841
Fullfifax <fullfifax1245@gmail.com>
2021-05-11 19:58:59 +0200

    register cote client

--- views/register.view.php
+++ views/register.view.php
@@ -1,5 +1,4 @@
 <?php 
-require("includes/constants.php");
 require_once("includes/_header.php"); 
 require_once("includes/_nav.php");
 ?>
@@ -16,10 +15,11 @@ require_once("includes/_nav.php");
             
             <div class="row">
                 
+                <?php include('includes/register_success.php'); ?>
                 <div class="col-md-12"></div>
                 
                 <h5 class="font-weight-normal ml-3">Would you like to register as</h5>
-                <div class="col-md-12"></div>
+                <div class="col-md-12 inputError mb-4"><?= $userError; ?></div>
                 
                 <div class="form-check ml-3">
                     <input class="form-check-input" type="radio" name="user" id="user" value="teacher">
@@ -29,19 +29,21 @@ require_once("includes/_nav.php");
                     <input class="form-check-input" type="radio" name="user" id="user1" value="student">
                     <label class="form-check-label" for="user1">Student</label>
                 </div>
+                <div class="col-md-12"></div>
 
-                <div class="col-md-8"></div>
+                <div class="col-md-8 inputError"><?= $usernameError; ?></div>
                 <input type="text" class="form-control ml-3 mb-4 col-md-4" id= "username" name="username" placeholder="Username">
                 
-                <div class="col-md-8"></div>
+                <div class="col-md-8 inputError"><?= $emailError; ?></div>
                 <input type="email" class="form-control ml-3 mb-4 col-md-4" name="email" placeholder="Email adress">
                 
-                <div class="col-md-8"></div>
+                <div class="col-md-8 inputError"><?= $passwordError; ?></div>
                 <input type="password" class="form-control ml-3 mb-4 col-md-4" name="password" placeholder="Password">
                 
-                <div class="col-md-8"></div>
+                <div class="col-md-8 inputError"><?= $passwordConfirmError; ?></div>
                 <input type="password"class="form-control ml-3 mb-4 col-md-4" name="password_confirm" placeholder="Confirm your password">
                 
+
                 <div class="col-md-8"></div>
                 <button type="submit" class="btn btn-lg btn-block ml-3 col-md-4" name="register" style="background-color:grey;">Sign-up</button>
                 
@@ -57,7 +59,7 @@ require_once("includes/_nav.php");
 
     </div>
 
-</div>
+    </div>
 
 </body>
 </html>
