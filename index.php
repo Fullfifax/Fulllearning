975c09bdaa6a4a9176e6957040417b2ccd0e598e
Fullfifax <fullfifax1245@gmail.com>
2021-05-11 15:02:25 +0200

    Page d'accueil

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fulllearning :)</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+        <h5 class="my-0 mr-md-auto font-weight-normal">Fulllearning :)</h5>
+        <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-dark" href="#">Login</a>
+            <a class="p-2 text-dark" href="#">Register</a>
+        </nav>
+    </div>
+
+    <div class="jumbotron p-3 p-md-5 text-white bg-dark">
+        <div class="col-md-6 px-0">
+          <h1 class="display-4 font-italic">Fulllearning :) "Study everywhere".</h1>
+          <p class="lead my-3">Fulllearning is a simple e-learning project which helps majority of students to have an access on their courses from their teachers/instructors.</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="p-3 mb-3">
+                <h4 class="font-italic">About Fulllearning :)</h4>
+                <p class="mb-0">
+                    Fulllearning :) helps majority of students to have an access on their courses. <br>
+                    And it is an opportunity for teachers/instructors to share/upload courses videos for their students. <br>
+                    You can <a href="#">create an account</a> or <a href="#">login</a> on Fulllearning :)
+                </p>
+        </div>
+    </div>
+   
+
+<script src="assets/js/jquery-3.5.1.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
