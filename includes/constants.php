10d054548e121f7b9bfa780989e9c3d181e9ec4f
Fullfifax <fullfifax1245@gmail.com>
2021-05-11 18:26:10 +0200

    Ajout des nouvelles constantes

--- includes/constants.php
+++ includes/constants.php
@@ -2,4 +2,9 @@
 
     define('WEBSITE_NAME', 'Fulllearning :)'); 
 
+    define('DB_NAME', 'fulllearning');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_HOST', 'localhost');
+
 ?>
