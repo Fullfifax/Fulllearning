0a859a83c8b0dd36a68057f56300bbc39b0e3fa8
Fullfifax <fullfifax1245@gmail.com>
2021-05-15 15:07:45 +0200

    Quelques modifications ....

--- courses/index.php
+++ courses/index.php
@@ -22,9 +22,9 @@ $rows = $stm->fetchAll(PDO::FETCH_ASSOC);
         <div class="col-sm-6 mb-4">
             <div class="card bg-dark text-light">
                 <div class="card-body">
-                    <h5 class="card-title"><?= $row['nom']; ?></h5>
-                    <p class="card-text"><?= $row['information']; ?></p>
-                    <a href="show.php?id=<?=$row['id'];?>" class="btn btn-light text-dark">Show more</a>
+                    <h5 class="card-title text-center"><?= $row['nom']; ?></h5>
+                    <p class="card-text text-justify"><?php echo substr($row['information'], 0, 150)."..."; ?></p>
+                    <a href="show.php?id=<?=$row['id'];?>" class="btn btn-light text-dark">Watch video</a>
                     <a href="delete.php?id=<?= $row['id']; ?>" class="btn btn-danger text-light ml-4">Delete</a>
                 </div>
             </div>
