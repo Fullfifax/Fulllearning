4c681987edf18d63f19cc68d96799d9a793d2b0f
Fullfifax <fullfifax1245@gmail.com>
2021-05-16 09:07:22 +0200

    Set title ...

--- courses/show.php
+++ courses/show.php
@@ -3,6 +3,8 @@ session_start();
 
 include("../filters/teacher_filter.php");
 
+$title = "Show course";
+
 require_once('../includes/constants.php');
 require_once('../config/database.php');
 require_once('../includes/_header.php');
