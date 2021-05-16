cbc447383c67098bc59231bd7168055eda31a873
Fullfifax <fullfifax1245@gmail.com>
2021-05-16 09:07:58 +0200

    Add condition ...

--- includes/_header.php
+++ includes/_header.php
@@ -3,7 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fulllearning :)</title>
+    <title>
+        <?=
+            isset($title)
+            ? $title." - FullLearning"
+            : WEBSITE_NAME
+        ?>
+    </title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/styles.css">
