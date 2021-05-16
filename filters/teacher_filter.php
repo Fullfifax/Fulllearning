11d63da013c78b12d1be88b449d5ae77b160a222
Fullfifax <fullfifax1245@gmail.com>
2021-05-16 08:41:07 +0200

    Teacher filter ....

--- filters/teacher_filter.php
+++ filters/teacher_filter.php
@@ -1,6 +1,6 @@
 <?php
 
-if(!isset($_SESSION['username']) && !isset($_SESSION['password'])){
+if(!isset($_SESSION['user_id']) && !isset($_SESSION['password'])){
     header('Location: ../login.php');
     exit();
 } 
